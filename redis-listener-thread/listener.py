03ed732ef9425856033ebf2901e5aee6bfd796e6
zeynepAydin2 <aydnzynp40@gmail.com>
2022-03-10 00:08:54 +0300

    tab to 4spc

--- redis-listener-thread/listener.py
+++ redis-listener-thread/listener.py
@@ -12,40 +12,40 @@ postgresql_con = psycopg2.connect(
     password="mysecretpassword")
 
 def record_db(usr):
-	cursor = postgresql_con.cursor()
+    cursor = postgresql_con.cursor()
 
-	first_name = usr[b'first_name'].decode()
-	last_name = usr[b'last_name'].decode()
-	email = usr[b'email'].decode()
-	gender = usr[b'gender'].decode()
-	ip_address = usr[b'ip_address'].decode()
-	user_name = usr[b'user_name'].decode()
-	agent = usr[b'agent'].decode()
-	country = usr[b'country'].decode()
+    first_name = usr[b'first_name'].decode()
+    last_name = usr[b'last_name'].decode()
+    email = usr[b'email'].decode()
+    gender = usr[b'gender'].decode()
+    ip_address = usr[b'ip_address'].decode()
+    user_name = usr[b'user_name'].decode()
+    agent = usr[b'agent'].decode()
+    country = usr[b'country'].decode()
 
-	cursor.execute("""INSERT INTO users(first_name, last_name, email, gender, ip_address, user_name, agent, country) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)""", (first_name, last_name, email, gender, ip_address, user_name, agent, country))
+    cursor.execute("""INSERT INTO users(first_name, last_name, email, gender, ip_address, user_name, agent, country) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)""", (first_name, last_name, email, gender, ip_address, user_name, agent, country))
 
-	try:
-		postgresql_con.commit()
-		cursor.close()
-		return True
-	except Exception as e:
-		print(e)
-		return False
+    try:
+        postgresql_con.commit()
+        cursor.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 def listen():
-	for key in redis_con.scan_iter():
-		email = key.decode()
-		data = redis_con.hgetall(email)
-		is_recorded = record_db(data)
-		if is_recorded == True:
-			redis_con.delete(email)
-		else:
-			print("FLASE")
-			pass
+    for key in redis_con.scan_iter():
+        email = key.decode()
+        data = redis_con.hgetall(email)
+        is_recorded = record_db(data)
+        if is_recorded == True:
+            redis_con.delete(email)
+        else:
+            print("FLASE")
+            pass
 
 if __name__ == "__main__":
-	while True:
-		listen()
-		time.sleep(10)
+    while True:
+        listen()
+        time.sleep(10)
 
--- upload-api/uploadapi.py
+++ upload-api/uploadapi.py
@@ -7,20 +7,20 @@ conn = redis.Redis('localhost')
 
 @app.route('/upload-file', methods=['POST'])
 def upload_file():
-	if request.method == 'POST':
-		jsonfile = request.files['jsonfile']
-		file_data = json.load(jsonfile)
-		error_list = []
-		for i in file_data:
-			tmp = conn.hmset(i['email'], i)
-			if tmp == False:
-				error_list.append(i['id'])
-		if len(error_list) != 0:
-			return str(error_list), 500	
-		else:
-			return "success", 200
-	else:
-		return "bad request", 400
+    if request.method == 'POST':
+        jsonfile = request.files['jsonfile']
+        file_data = json.load(jsonfile)
+        error_list = []
+        for i in file_data:
+            tmp = conn.hmset(i['email'], i)
+            if tmp == False:
+                error_list.append(i['id'])
+        if len(error_list) != 0:
+            return str(error_list), 500 
+        else:
+            return "success", 200
+    else:
+        return "bad request", 400
   
 if __name__ == '__main__':
     app.run()
