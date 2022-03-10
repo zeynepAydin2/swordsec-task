ac9d150596590b947c9126c5fe70eb1633cc211d
zeynepAydin2 <aydnzynp40@gmail.com>
2022-03-11 00:43:03 +0300

    comments added to codes

--- redis-listener-thread/listener.py
+++ redis-listener-thread/listener.py
@@ -3,8 +3,10 @@ import json
 import redis
 import time
 
+# Redis connection
 redis_con = redis.Redis('localhost')
 
+# PostgreSQL connection
 postgresql_con = psycopg2.connect(
     host="localhost",
     user="postgres",
@@ -13,7 +15,8 @@ postgresql_con = psycopg2.connect(
 
 def record_db(usr):
     cursor = postgresql_con.cursor()
-
+    
+    # usr objesindeki her bir value icin byte -> str yapilir.
     first_name = usr[b'first_name'].decode()
     last_name = usr[b'last_name'].decode()
     email = usr[b'email'].decode()
@@ -23,28 +26,40 @@ def record_db(usr):
     agent = usr[b'agent'].decode()
     country = usr[b'country'].decode()
 
+    # data postgresqle insert edilir.
     cursor.execute("""INSERT INTO users(first_name, last_name, email, gender, ip_address, user_name, agent, country) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)""", (first_name, last_name, email, gender, ip_address, user_name, agent, country))
-
+    
     try:
+        # insert komutu commit edilir
         postgresql_con.commit()
         cursor.close()
         return True
     except Exception as e:
+        # commit esnasinda hata ile karsilasilirsa False doner
         print(e)
         return False
 
 def listen():
+    # scan_iter metodu ile redis'de bulunan tum keyler alinir
     for key in redis_con.scan_iter():
+        
+        # email byte -> str
         email = key.decode()
+        
+        # key icin data okunur
         data = redis_con.hgetall(email)
+
+        # postgresql'e yazilmak uzere record_db fonksiyonuna gonderilir
         is_recorded = record_db(data)
+
+        # Veriler kaydedilmisse silinir.
         if is_recorded == True:
             redis_con.delete(email)
         else:
-            print("FLASE")
             pass
 
 if __name__ == "__main__":
+    # her 10 saniyede redis kontrol edilir.
     while True:
         listen()
         time.sleep(10)
--- upload-api/uploadapi.py
+++ upload-api/uploadapi.py
@@ -2,19 +2,34 @@ from flask import Flask, request
 import json
 import redis
 
+# flask app olusturuldu.
 app = Flask(__name__)
+
+# Redise connection olusturuldu.
 conn = redis.Redis('localhost')
 
+# /upload-api endpointi
 @app.route('/upload-file', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
+        # request icerisindeki dosya alindi
         jsonfile = request.files['jsonfile']
+        
+        # Dosya icerigi json'a cevirildi.
         file_data = json.load(jsonfile)
+
+        # hata ile karsilasilmasi durumunda kullaniciya donulecek id listesi
         error_list = []
+
+        # dosya icerigindeki her bir veri redise yazilir
         for i in file_data:
-            tmp = conn.hmset(i['email'], i)
+            tmp = conn.hmset(i['email'], i)ı
+
+            # redise yazilamamasi durumunda id error_list e eklenir
             if tmp == False:
                 error_list.append(i['id'])
+        
+        # error_list bos degilse 500 bos ise 200 
         if len(error_list) != 0:
             return str(error_list), 500 
         else:
